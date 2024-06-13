b8bf4d687c806aa8ac9fc8d4aae64be4e9e921ab
RuiwenTang <tangruiwen1989@gmail.com>
2024-06-13 19:41:03 +0800

    draw eye background color

--- examples/nanovg-example.rs
+++ examples/nanovg-example.rs
@@ -1,8 +1,11 @@
+use std::time;
+
 use mickey::*;
 
 mod common;
 
 struct NanovgRender {
+    begin_time: time::Instant,
     width: f32,
     height: f32,
     context: Option<GPUContext>,
@@ -11,14 +14,84 @@ struct NanovgRender {
 impl NanovgRender {
     fn new(width: f32, height: f32) -> Self {
         Self {
+            begin_time: time::Instant::now(),
             width,
             height,
             context: None,
         }
     }
 
+    fn draw_eyes(
+        &self,
+        recorder: &mut PictureRecorder,
+        x: f32,
+        y: f32,
+        w: f32,
+        h: f32,
+        mx: f32,
+        my: f32,
+        t: f64,
+    ) {
+        let ex = w * 0.23;
+        let ey = h * 0.5;
+        let lx = x + ex;
+        let ly = y + ey;
+        let rx = x + w - ex;
+        let ry = y + ey;
+        let br = if ex < ey { ex * 0.5 } else { ey * 0.5 };
+        let blink = 1.0 - (t * 0.5).sin().powf(200.0) * 0.8;
+
+        let mut bg = Paint::new();
+        bg.style = Style::Fill;
+        bg.color =
+            LinearGradient::new(Point::from(x, y + h * 0.5), Point::from(x + w * 0.1, y + h))
+                .with_colors(vec![
+                    Color::from_rgba_u8(0, 0, 0, 32),
+                    Color::from_rgba_u8(0, 0, 0, 16),
+                ])
+                .into();
+
+        recorder.draw_oval(
+            &Rect::from_ltrb(lx + 0.3 - ex, ly + 16.0 - ey, lx + 3.0 + ex, ly + 16.0 + ey),
+            &bg,
+        );
+
+        recorder.draw_oval(
+            &Rect::from_ltrb(rx + 3.0 - ex, ry + 16.0 - ey, rx + 3.0 + ex, ry + 16.0 + ey),
+            &bg,
+        );
+
+        bg.color = LinearGradient::new(
+            Point::from(x, y + h * 0.25),
+            Point::from(x + w * 0.1, y + h),
+        )
+        .with_colors(vec![
+            Color::from_rgba_u8(220, 220, 220, 255),
+            Color::from_rgba_u8(128, 128, 128, 255),
+        ])
+        .into();
+
+        recorder.draw_oval(&Rect::from_ltrb(lx - ex, ly - ey, lx + ex, ly + ey), &bg);
+        recorder.draw_oval(&Rect::from_ltrb(rx - ex, ry - ey, rx + ex, ry + ey), &bg);
+    }
+
     fn render(&self) -> Picture {
+        let current = time::Instant::now();
+
+        let delta = (current - self.begin_time).as_millis() as f64;
+
         let mut recorder = PictureRecorder::new();
+
+        self.draw_eyes(
+            &mut recorder,
+            self.width - 250.0,
+            50.0,
+            150.0,
+            100.0,
+            0.0,
+            0.0,
+            delta,
+        );
 
         return recorder.finish_record();
     }
